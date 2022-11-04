f257a615382769144e455e1758606a4a20210c3c
Sonya0932 <108592540+Sonya0932@users.noreply.github.com>
2022-11-04 21:38:32 +0200

    add js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form')
+form.addEventListener('submit', onFormSubmit)
 
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElem = event.currentTarget.elements;
+    console.log(formElem)
+
+    if (formElem.email.value === '' || formElem.password.value === '') {
+        return alert('Усі поля повинні бути заповнені')
+    }
+
+    const email = formElem.email.value;
+    const password = formElem.password.value;
+
+    const formData = {
+        email,
+        password,
+    }
+    console.log(formData)
+    event.currentTarget.reset()
+}
